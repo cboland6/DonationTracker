f79ae07262b3e4a34bcc30aba9ad197ab1dfaf53
caseyanne4 <cboland6@gatech.edu>
2018-09-21 12:27:39 -0400

    cancel button goes back to the welcome screen

--- app/src/main/java/com/example/casey/donationtracker/LoginScreen.java
+++ app/src/main/java/com/example/casey/donationtracker/LoginScreen.java
@@ -1,7 +1,10 @@
 package com.example.casey.donationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        goBackToWelcome();
+    }
+
+    private void goBackToWelcome() {
+        Button backButton = (Button) findViewById(R.id.cancelbutton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this, MainActivity.class));
+            }
+        });
     }
 }

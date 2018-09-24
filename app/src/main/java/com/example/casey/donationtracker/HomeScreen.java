0b2e39c2fb272f869202b25dec6701e8b2f20281
caseyanne4 <cboland6@gatech.edu>
2018-09-24 19:32:11 -0400

    logout button works

--- app/src/main/java/com/example/casey/donationtracker/HomeScreen.java
+++ app/src/main/java/com/example/casey/donationtracker/HomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.casey.donationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        configureLogOutButton();
+    }
+
+    private void configureLogOutButton() {
+        Button logOutButton = (Button) findViewById(R.id.LogOut);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this, MainActivity.class));
+            }
+        });
     }
 }

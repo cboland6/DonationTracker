1d432d425a738490dd1d310a34e0644488b105a5
caseyanne4 <cboland6@gatech.edu>
2018-09-30 18:22:42 -0400

    registration button brings you to registration screen

--- app/src/main/java/com/example/casey/donationtracker/RegScreen.java
+++ app/src/main/java/com/example/casey/donationtracker/RegScreen.java
@@ -1,4 +1,25 @@
 package com.example.casey.donationtracker;
 
-public class RegScreen {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegScreen extends AppCompatActivity {
+
+    // Widgets
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_screen);
+
+    // grab widgets
+    }
+
+    //need register method here
+
+
 }

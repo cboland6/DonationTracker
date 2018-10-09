3c08bca88c8a79538c3c259a3cb4ba733c4f4c27
onsideiut <42740586+onsideiut@users.noreply.github.com>
2018-10-09 13:38:59 -0400

    Update Location.java
    Combines address information to return one full address.
    Stores all elements in Strings to avoid parsing errors. Data should be converted to ints later if neccesary.

--- app/src/main/java/com/example/casey/donationtracker/Model/Location.java
+++ app/src/main/java/com/example/casey/donationtracker/Model/Location.java
@@ -2,19 +2,19 @@ package com.example.casey.donationtracker.Model;
 
 public class Location {
     private String name;
-    private int latitude;
-    private int longitude;
+    private String latitude;
+    private String longitude;
     private String streetAddress;
     private String city;
     private String state;
-    private int zip;
+    private String zip;
     private String type;
     private String phone;
     private String website;
     private String Address;
 
-    public Location(String n, int lat, int lo, String sA,
-                    String c, String s, int z, String t, String p, String w) {
+    public Location(String n, String lat, String lo, String sA,
+                    String c, String s, String z, String t, String p, String w) {
         name = n;
         latitude = lat;
         longitude = lo;
@@ -32,8 +32,8 @@ public class Location {
     public String toString() { return name; }
 
     public String getLocationName() { return name; }
-    public int getLatitude() { return latitude; }
-    public int getLongitude() { return longitude; }
+    public String getLatitude() { return latitude; }
+    public String getLongitude() { return longitude; }
     public String getAddress() { return Address; }
     public String getLocationType() { return type; }
     public String getLocationPhone() { return phone; }
